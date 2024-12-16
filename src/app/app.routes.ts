09f87cc402d0a5575ab3879037ca5d16bfa2b3d5
Joel Smith <joelsmith.2019@gmail.com>
2024-12-15 20:31:15 -0600

    Structure Website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TosComponent } from './pages/tos/tos.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'tos', component: TosComponent },
+    { path: '**', component: NotFoundComponent }
+];
