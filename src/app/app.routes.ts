33c5b5cb4db459511e9db821ff32d01f3f76245d
Joel Smith <joelsmith.2019@gmail.com>
2024-12-23 11:53:57 -0600

    Add Privacy Policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { TosComponent } from './pages/tos/tos.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'terms-of-service', component: TosComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: '**', component: NotFoundComponent }
 ];
