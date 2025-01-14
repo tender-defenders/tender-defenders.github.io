1a6f2f8a1bdf1feb5a862d03921d34febce3dba0
Joel Smith <joelsmith.2019@gmail.com>
2025-01-14 16:29:08 -0600

    Add Deep Link URLs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { TosComponent } from './pages/tos/tos.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { ConfirmEmailComponent } from './pages/confirm-email/confirm-email.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'terms-of-service', component: TosComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    { path: 'reset-password', component: ResetPasswordComponent },
+    { path: 'confirm-email', component: ConfirmEmailComponent },
     { path: '**', component: NotFoundComponent }
 ];
---
+++ src/app/pages/confirm-email/confirm-email.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-email',
+  imports: [],
+  templateUrl: './confirm-email.component.html',
+  styleUrl: './confirm-email.component.scss'
+})
+export class ConfirmEmailComponent {
+
+}
